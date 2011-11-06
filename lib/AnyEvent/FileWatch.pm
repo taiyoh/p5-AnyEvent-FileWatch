48189a27f1c4d29a55d02ae1fae44a81bc34f59e
taiyoh <sun.basix@gmail.com>
2011-11-06 17:32:23 +0900

    initial commit

---
+++ lib/AnyEvent/FileWatch.pm
@@ -0,0 +1,32 @@
+package AnyEvent::FileWatch;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+AnyEvent::FileWatch -
+
+=head1 SYNOPSIS
+
+  use AnyEvent::FileWatch;
+
+=head1 DESCRIPTION
+
+AnyEvent::FileWatch is
+
+=head1 AUTHOR
+
+Taiyoh Tanaka E<lt>sun.basix@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
